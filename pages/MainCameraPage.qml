sailfishos/jolla-camera

commit abe4f36687e8207e34f02f8165bff5fac230bd1c

[jolla-camera] Add support for multiple cameras. Fixes JB#54277

pages/MainCameraPage.qml
@@ -53,7 +53,12 @@ CameraPage {
             if (switchToImageMode) {
                 Settings.global.captureMode = "image"
             }
-            Settings.cameraDevice = "secondary"
+            if (Settings.frontFacingDeviceId >= 0) {
+                Settings.deviceId = Settings.frontFacingDeviceId
+            } else {
+                console.warn("No front camera detected")
+            }
+
             page.returnToCaptureMode()
             window.activate()
         }

src/capture/CaptureView.qml
@@ -105,7 +105,7 @@ FocusScope {
     readonly property bool _applicationActive: Qt.application.state == Qt.ApplicationActive
     on_ApplicationActiveChanged: if (_applicationActive) flashlightServiceProbe.checkFlashlightServiceStatus()
 
-    readonly property string cameraDevice: Settings.cameraDevice
+    readonly property string deviceId: Settings.deviceId
 
     property var captureOverlay: null
 
@@ -174,6 +174,64 @@ FocusScope {
         }
     }
 
+
+    function _pickViewfinderResolution(resolutions, aspectRatio) {
+        var ratio
+        if (aspectRatio === CameraConfigs.AspectRatio_16_9) {
+            ratio = 16.0/9.0
+        } else { // CameraConfigs.AspectRatio_4_3
+            ratio = 4.0/3.0
+        }
+
+        if (resolutions && resolutions.length > 0) {
+            var selectedPixels = 0
+            var selectedIndex = 0
+            var targetWidth = Math.round(Screen.width * ratio)
+            for (var i = 0; i < resolutions.length; i++) {
+                var resolution = resolutions[i]
+                if (resolution.height === Screen.width && resolution.width === targetWidth) {
+                    return resolution
+                }
+            }
+            return _pickResolution(resolutions, aspectRatio)
+        }
+        return "-1x-1"
+    }
+
+    function aspectRatioToFraction(aspectRatio) {
+
+        var ratio = 4.0/3.0
+        if (aspectRatio === CameraConfigs.AspectRatio_16_9) {
+            ratio = 16.0/9.0
+        } else if (aspectRatio !== CameraConfigs.AspectRatio_4_3) {
+            console.warn("Unknown aspect ratio", aspectRatio)
+        }
+        return ratio
+    }
+
+    function _pickResolution(resolutions, aspectRatio) {
+
+        var ratio = aspectRatioToFraction(aspectRatio)
+
+        if (resolutions && resolutions.length > 0) {
+            var selectedPixels = 0
+            var selectedIndex = -1
+            for (var i = 0; i < resolutions.length; i++) {
+                var resolution = resolutions[i]
+                var pixels = resolution.width * resolution.height
+                if (Math.abs(ratio - resolution.width/resolution.height) < 0.05 && pixels > selectedPixels) {
+                    selectedPixels = pixels
+                    selectedIndex = i
+                }
+            }
+
+            if (selectedIndex >= 0) {
+                return resolutions[selectedIndex]
+            }
+        }
+        return "-1x-1"
+    }
+
     Notification {
         id: microphoneWarningNotification
 
@@ -206,20 +264,18 @@ FocusScope {
 
     Component.onCompleted: {
         flashlightServiceProbe.checkFlashlightServiceStatus()
-        camera.deviceId = Settings.mode.cameraId >= 0
-                          ? Settings.mode.cameraId
-                          : Settings.global.cameraDevice === "primary" ? 0 : 1
         loadOverlay()
-        _complete = true
     }
 
-    onCameraDeviceChanged: {
+    onDeviceIdChanged: {
         _resetFocus()
         captureTimer.reset()
-        Settings.global.cameraDevice = Settings.cameraDevice
-        camera.deviceId = Settings.mode.cameraId >= 0
-                          ? Settings.mode.cameraId
-                          : Settings.cameraDevice === "primary" ? 0 : 1
+        Settings.global.deviceId = Settings.deviceId
+        camera.deviceId = Settings.deviceId
+        Settings.global.position = camera.position
+        if (camera.position === Camera.BackFace) {
+            Settings.global.previousBackFacingDeviceId = camera.deviceId
+        }
     }
 
     onEffectiveActiveChanged: {
@@ -404,19 +460,34 @@ FocusScope {
             }
         }
 
-        captureMode: Settings.mode.captureMode
+        property bool hasCameraOnBothSides
+        property string frontFacingDeviceId
+        property var backFacingCameras
+
+        deviceId: Settings.deviceId
+        captureMode: Settings.global.captureMode == "image" ? Camera.CaptureStillImage
+                                                            : Camera.CaptureVideo
 
         onCaptureModeChanged: captureView._resetFocus()
 
-        cameraState: captureView._complete && captureView.effectiveActive && !captureView._unload
-                    ? Camera.ActiveState
-                    : Camera.UnloadedState
+        cameraState: {
+            if (captureView.effectiveActive && !captureView._unload) {
+                if (CameraConfigs.ready) {
+                    return Camera.ActiveState
+                } else {
+                    return Camera.LoadedState
+                }
+            } else {
+                  return Camera.UnloadedState
+            }
+        }
 
         onCameraStateChanged: {
             if (cameraState == Camera.ActiveState && captureOverlay) {
                 captureView.loaded()
             }
         }
+        property bool initialized
 
         onCameraStatusChanged: {
             if (camera.cameraStatus == Camera.StartingStatus) {
@@ -429,10 +500,44 @@ FocusScope {
                 _captureQueued = false
                 captureBusy = false
             }
+
+            var backCameras = []
+            if (cameraStatus === Camera.LoadingStatus && !initialized) {
+                initialized = true
+                var hasFrontFace = false
+                var hasBackFace = false
+
+
+                for (var i = 0; i < QtMultimedia.availableCameras.length; i++) {
+                    var device = QtMultimedia.availableCameras[i]
+                    if (device.position === Camera.FrontFace) {
+                        hasFrontFace = true
+                        frontFacingDeviceId = device.deviceId
+                    } else if (device.position === Camera.BackFace) {
+                        hasBackFace = true
+                        backCameras.push(device)
+                    }
+                    if (device.position === Camera.FrontFace) {
+                        break
+                    }
+                }
+
+                backFacingCameras = backCameras
+
+                hasCameraOnBothSides = hasFrontFace && hasBackFace
+
+                if (Settings.global.previousBackFacingDeviceId.length === 0 && backCameras.length > 0) {
+                    if (backCameras.indexOf(QtMultimedia.defaultCamera.deviceId) >= 0) {
+                        Settings.global.previousBackFacingDeviceId = QtMultimedia.defaultCamera.deviceId
+                    } else {
+                        Settings.global.previousBackFacingDeviceId = backCameras[0].deviceId
+                    }
+                }
+            }
         }
 
         imageCapture {
-            resolution: Settings.mode.imageResolution
+            resolution: _pickResolution(CameraConfigs.supportedImageResolutions, Settings.aspectRatio)
             onResolutionChanged: reload()
 
             onImageSaved: {
@@ -471,9 +576,9 @@ FocusScope {
             }
         }
         videoRecorder {
-            resolution: Settings.mode.videoResolution
+            resolution: _pickResolution(CameraConfigs.supportedVideoResolutions, CameraConfigs.AspectRatio_16_9)
+
             onResolutionChanged: reload()
-            frameRate: Settings.mode.videoFrameRate
             audioChannels: 2
             audioSampleRate: Settings.global.audioSampleRate
             audioCodec: Settings.global.audioCodec
@@ -494,10 +599,12 @@ FocusScope {
                 // real support was unknown at that time.
                 if (camera.cameraStatus == Camera.ActiveStatus && tapFocusActive) {
                     return Camera.FocusAuto
-                } else if (Settings.mode.focusDistanceValues.indexOf(Camera.FocusContinuous) >= 0) {
+                } else if (CameraConfigs.supportedFocusModes.indexOf(Camera.FocusContinuous) >= 0) {
                     return Camera.FocusContinuous
+                } else if (CameraConfigs.supportedFocusModes.length > 0) {
+                    return CameraConfigs.supportedFocusModes[0]
                 } else {
-                    return Settings.mode.focusDistanceValues[0]
+                    return Camera.FocusAuto
                 }
             }
             focusPointMode: tapFocusActive ? Camera.FocusPointCustom : Camera.FocusPointAuto
@@ -512,7 +619,14 @@ FocusScope {
         }
 
         viewfinder {
-            resolution: Settings.mode.viewfinderResolution
+            resolution: {
+                var resolutions = CameraConfigs.supportedViewfinderResolutions
+                if (resolutions.length > 0) {
+                    return _pickViewfinderResolution(resolutions, Settings.aspectRatio)
+                }
+                return "-1x-1"
+            }
+
             // Let gst-droid decide the best framerate
         }
 
@@ -522,7 +636,6 @@ FocusScope {
             cameraManufacturer: deviceInfo.manufacturer
         }
 
-        onDeviceIdChanged: captureView.reload()
         viewfinder.onResolutionChanged: captureView.reload()
         focus.onFocusModeChanged: camera.unlock()
 
@@ -532,6 +645,12 @@ FocusScope {
                 camera._completeCapture()
             }
         }
+    }
+
+    Binding {
+        target: CameraConfigs
+        property: "camera"
+        value: camera
     }
 
     DeviceInfo {

src/settings.qml
@@ -1,21 +1,23 @@
 import QtQuick 2.0
-import QtMultimedia 5.0
+import QtMultimedia 5.6
 import org.nemomobile.configuration 1.0
 import com.jolla.camera 1.0
 
 SettingsBase {
     property alias mode: modeSettings
     property alias global: globalSettings
-    // mode change goes here, CaptureView updates to global.cameraDevice
-    property string cameraDevice: global.cameraDevice
+    // Camera change goes here, CaptureView updates to global.deviceId
+    property string deviceId: global.deviceId
+
+    readonly property int aspectRatio: mode.aspectRatio
 
     readonly property var settingsDefaults: ({
                                                  "iso": 0,
                                                  "timer": 0,
                                                  "viewfinderGrid": "none",
                                                  "exposureMode": Camera.ExposureManual,
-                                                 "flash": ((modeSettings.captureMode == Camera.CaptureStillImage) &&
-                                                           (globalSettings.cameraDevice === "primary") ?
+                                                 "flash": ((globalSettings.captureMode == "image") &&
+                                                           (globalSettings.position === Camera.BackFace) ?
                                                                Camera.FlashAuto : Camera.FlashOff)
                                              })
 
@@ -24,11 +26,6 @@ SettingsBase {
                                             modeSettings.viewfinderGrid === settingsDefaults["viewfinderGrid"] &&
                                             modeSettings.exposureMode === settingsDefaults["exposureMode"] &&
                                             modeSettings.flash == settingsDefaults["flash"]
-    // FIXME: should use something like QtMultimedia.availableCameras.length > 1, but that currently
-    // returns bogus cameras on Sailfish when they don't really exist.
-    // Also now only handling missing back camera which is somewhat hard-coded in qtmultimedia gstreamer integration
-    // to be the primary camera.
-    readonly property bool hasMultipleCameras: primaryResolution.value != "" && primaryResolution.value[0] != "0"
 
     function reset() {
         var basePath = globalSettings.path + "/" + modeSettings.path
@@ -40,11 +37,6 @@ SettingsBase {
     }
 
     property ConfigurationValue _singleValue: ConfigurationValue {}
-    property ConfigurationValue _primaryCameraResolution: ConfigurationValue {
-        id: primaryResolution
-        key: globalSettings.path + "/primary/image/imageResolution"
-        defaultValue: ""
-    }
 
     property ConfigurationGroup _global: ConfigurationGroup {
         id: globalSettings
@@ -52,8 +44,14 @@ SettingsBase {
         path: "/apps/jolla-camera"
 
         // Note! don't touch this for changing between cameras, see cameraDevice on root
-        property string cameraDevice: "primary"
+        property string deviceId
+        property string previousBackFacingDeviceId
+        property int position: Camera.BackFace
         property string captureMode: "image"
+
+        // Need to be defined by adaptation to enable multiple back cameras,
+        // e.g. normal, macro and wide angle camera labels could be ["1.0", "2.0", "0.6"]
+        property var backCameraLabels: []
 
         property int portraitCaptureButtonLocation: 3
         property int landscapeCaptureButtonLocation: 5
@@ -74,20 +72,14 @@ SettingsBase {
         property int whiteBalance: CameraImageProcessing.WhiteBalanceAuto
 
         property var exposureCompensationValues: [ 4, 3, 2, 1, 0, -1, -2, -3, -4 ]
-        property var whiteBalanceValues: [
-            CameraImageProcessing.WhiteBalanceAuto,
-            CameraImageProcessing.WhiteBalanceCloudy,
-            CameraImageProcessing.WhiteBalanceSunlight,
-            CameraImageProcessing.WhiteBalanceFluorescent,
-            CameraImageProcessing.WhiteBalanceTungsten
-        ]
 
         ConfigurationGroup {
             id: modeSettings
-            path: globalSettings.cameraDevice + "/" + globalSettings.captureMode
 
-            property int cameraId: -1
-            property int captureMode: Camera.CaptureStillImage
+            path: {
+                var position = globalSettings.position === Camera.FrontFace ? "front" : "back"
+                return position + "/" + globalSettings.captureMode
+            }
 
             property int iso: 0
             property int flash: Camera.FlashOff
@@ -95,28 +87,18 @@ SettingsBase {
             property int meteringMode: Camera.MeteringMatrix
             property int timer: 0
             property string viewfinderGrid: "none"
-
-            property string imageResolution: "1280x720"
-            property string videoResolution: "1280x720"
-            property int videoFrameRate: 30
-            property string viewfinderResolution: "1280x720"
-
-            property var isoValues: [ 0, 100, 200, 400 ]
-            property var focusDistanceValues: [ Camera.FocusInfinity ]
-            property var flashValues: [ Camera.FlashOff ]
-            property var exposureModeValues: [
-                Camera.ExposureManual,
-                Camera.ExposureNight,
-                Camera.ExposurePortrait,
-                Camera.ExposureSports,
-                Camera.ExposureHDR
-            ]
-            property var meteringModeValues: [
-                Camera.MeteringMatrix,
-                Camera.MeteringAverage,
-                Camera.MeteringSpot
-            ]
             property var viewfinderGridValues: [ "none", "thirds", "ambience" ]
+            property int aspectRatio: -1
+
+            Component.onCompleted: {
+                if (aspectRatio === -1) {
+                    if (globalSettings.captureMode === "image") {
+                        aspectRatio = CameraConfigs.AspectRatio_4_3
+                    } else {
+                        aspectRatio = CameraConfigs.AspectRatio_16_9
+                    }
+                }
+            }
         }
     }
 
@@ -182,6 +164,8 @@ SettingsBase {
         case Camera.ExposureNight:          return "image://theme/icon-camera-mode-night"
         case Camera.ExposureSports:         return "image://theme/icon-camera-mode-sports"
         case Camera.ExposureHDR:            return "image://theme/icon-camera-mode-hdr"
+        default:
+            return "" // not supported
         }
     }
 
@@ -202,6 +186,8 @@ SettingsBase {
         //: "HDR exposure mode"
         //% "HDR exposure"
         case Camera.ExposureHDR:            return qsTrId("camera_settings-la-exposure-hdr")
+        default:
+            return "" // not supported
         }
     }
 
@@ -211,7 +197,10 @@ SettingsBase {
         case Camera.FlashOff:               return "image://theme/icon-camera-flash-off"
         case Camera.FlashTorch:
         case Camera.FlashOn:                return "image://theme/icon-camera-flash-on"
-        case Camera.FlashRedEyeReduction:   return "image://theme/icon-camera-flash-redeye"
+        // JB#54201: Red-eye mode does not work
+        // case Camera.FlashRedEyeReduction:   return "image://theme/icon-camera-flash-redeye"
+        default:
+            return "" // not supported
         }
     }
 
@@ -232,6 +221,8 @@ SettingsBase {
         //: "Camera flash with red eye reduction"
         //% "Flash red eye"
         case Camera.FlashRedEyeReduction: return qsTrId("camera_settings-la-flash-redeye")
+        default:
+            return "" // not supported
         }
     }
 
@@ -240,11 +231,12 @@ SettingsBase {
         case CameraImageProcessing.WhiteBalanceAuto:        return "image://theme/icon-camera-wb-automatic"
         case CameraImageProcessing.WhiteBalanceSunlight:    return "image://theme/icon-camera-wb-sunny"
         case CameraImageProcessing.WhiteBalanceCloudy:      return "image://theme/icon-camera-wb-cloudy"
-        case CameraImageProcessing.WhiteBalanceShade:       return "image://theme/icon-camera-wb-shade"
-        case CameraImageProcessing.WhiteBalanceSunset:      return "image://theme/icon-camera-wb-sunset"
+        // case CameraImageProcessing.WhiteBalanceShade:       return "image://theme/icon-camera-wb-shade"
+        // case CameraImageProcessing.WhiteBalanceSunset:      return "image://theme/icon-camera-wb-sunset"
         case CameraImageProcessing.WhiteBalanceFluorescent: return "image://theme/icon-camera-wb-fluorecent"
         case CameraImageProcessing.WhiteBalanceTungsten:    return "image://theme/icon-camera-wb-tungsten"
-        default: return "image://theme/icon-camera-wb-default"
+        default:
+            return "" // not supported
         }
     }
 
@@ -271,7 +263,8 @@ SettingsBase {
         //: "Tungsten white balance"
         //% "Tungsten"
         case CameraImageProcessing.WhiteBalanceTungsten:    return qsTrId("camera_settings-la-wb-tungsten")
-        default: return ""
+        default:
+            return "" // not supported
         }
     }
 

commit 266c5a7446f0eed9bef7c3fb4a044140e45b4264

[camera] Fix selfie action not changing to front camera. JB#61834
Looks like a regression in commit abe4f36687e8 which heavily modified
the camera setup. Halfway assuming the secondary camera id is found
from settings but then adding it inside a subview.

pages/MainCameraPage.qml
@@ -66,8 +66,8 @@ CameraPage {
             if (switchToImageMode) {
                 Settings.global.captureMode = "image"
             }
-            if (Settings.frontFacingDeviceId >= 0) {
-                Settings.deviceId = Settings.frontFacingDeviceId
+            if (Settings.global.frontFacingDeviceId !== "") {
+                Settings.deviceId = Settings.global.frontFacingDeviceId
             } else {
                 console.warn("No front camera detected")
             }

src/capture/CaptureView.qml
@@ -476,7 +476,6 @@ FocusScope {
         }
 
         property bool hasCameraOnBothSides
-        property string frontFacingDeviceId
         property var backFacingCameras
 
         // On some adaptations media booster makes camera initialization fail
@@ -547,7 +546,7 @@ FocusScope {
                     var device = QtMultimedia.availableCameras[i]
                     if (!hasFrontFace && device.position === Camera.FrontFace) {
                         hasFrontFace = true
-                        frontFacingDeviceId = device.deviceId
+                        Settings.global.frontFacingDeviceId = device.deviceId
                     } else if (device.position === Camera.BackFace) {
                         hasBackFace = true
                         backCameras.push(device)

src/settings.qml
@@ -52,6 +52,7 @@ SettingsBase {
         // Note! don't touch this for changing between cameras, see cameraDevice on root
         property string deviceId
         property string previousBackFacingDeviceId
+        property string frontFacingDeviceId
         property int position: Camera.BackFace
         property string captureMode: "image"
 

src/settings/SettingsOverlay.qml
@@ -162,7 +162,7 @@ PinchArea {
         visible: opacity > 0.0
                  && !!model && model.length > 1
                  && labels.length > 0
-                 && Settings.deviceId !== camera.frontFacingDeviceId
+                 && Settings.deviceId !== Settings.global.frontFacingDeviceId
                  && !inButtonLayout
         orientation: overlay.isPortrait ? Qt.Horizontal : Qt.Vertical
         enabled: camera.cameraStatus === Camera.ActiveStatus
@@ -193,7 +193,7 @@ PinchArea {
         anchors.centerIn: parent
         onClicked: {
             if (Settings.global.position === Camera.BackFace) {
-                Settings.deviceId = camera.frontFacingDeviceId
+                Settings.deviceId = Settings.global.frontFacingDeviceId
             } else {
                 Settings.deviceId = Settings.global.previousBackFacingDeviceId
             }